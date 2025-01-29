104833e5386aa2eaa27be400fb19824f468b1df3
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-28 22:11:21 -0500

    Clases del componente ChangeAlert

--- src/ChangeAlert/index.jsx
+++ src/ChangeAlert/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import './ChangeAlert.css'
 import { useStorageListener } from "./useStorageListener";
 function ChangeAlert({ sincronize }) {
     const { show, toggleShow } = useStorageListener(sincronize)
     if (show) {
         return (
-            <div>
-                <p>Hubo cambios</p>
+            <div className='ChangeAlert'>
+                <p>
+                    Parece que hubo algun cambio en algun ToDo en una 
+                    ventana alterna
+                </p>
                 <button
                   onClick={() => toggleShow(false)}
                   >
